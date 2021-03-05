c9a499246105f6bba0bf5b6b99f9bde6d5caa2e9
Ashley Edmondson <ace45611@creighton.edu>
2021-03-05 15:36:47 -0600

    Create for.js

---
+++ forms/for/for.js
@@ -0,0 +1,9 @@
+/*
+program uses a 'for' loop and an 'array' 
+that outputs each name in the array on its' own line
+*/
+
+let names = ['Bob', 'Janet', 'Tom', 'Erin', 'Lucy', 'Alyssa', 'Dustin']
+
+for (i = 0; i < names.length; i++)
+    console.log(names[i])
